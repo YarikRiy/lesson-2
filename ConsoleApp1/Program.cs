af81948d2084117cda769e37cfebde4fc7693cb0
yaroslav <riy19998@gmail.com>
2023-03-17 10:30:55 +0200

    ASAS

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@ internal class Program
     {
         //https://gitlab.com/study6903311/lesson-1.git
         Console.WriteLine("just hello world)");
+        // other something
         //something 123
     }
 }
