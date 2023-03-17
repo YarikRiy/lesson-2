39870f378375a4a5e82147208f23f61a96dff94f
yaroslav <riy19998@gmail.com>
2023-03-17 09:32:45 +0200

    add new feature

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,9 @@
 ﻿
-
-
-//https://gitlab.com/study6903311/lesson-1.git
-Console.WriteLine("just hello world)");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //https://gitlab.com/study6903311/lesson-1.git
+        Console.WriteLine("just hello world)");
+    }
+}
